98b9a7c7e2957d0f7325cd2d8a74e18bc6109d9f
Esther-Julius <100392086+Esther-Julius@users.noreply.github.com>
2022-05-21 17:29:13 +0100

    speakBye.js

---
+++ speakBye.js
@@ -0,0 +1,10 @@
+var  byeSpeaker = function speak(){
+    var speakWord = "Good Bye";
+
+    function speak(name){
+        console.log(speakWord +"" + name);
+    }
+    name.byeSpeaker = byeSpeaker;
+}
+(name);
+
