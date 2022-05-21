7e6f0b18eabc27b2947e5dcf4986e791f99d7b18
Esther-Julius <100392086+Esther-Julius@users.noreply.github.com>
2022-05-21 17:26:40 +0100

    speakHello.js

---
+++ speakHello.js
@@ -0,0 +1,10 @@
+var helloSpeaker = function (){
+    var speakWord = "Hello";
+
+    function speak(name){
+        console.log(speakWord +"" + name);
+    }
+    name.helloSpeaker = helloSpeaker
+}
+(helloSpeaker);
+
